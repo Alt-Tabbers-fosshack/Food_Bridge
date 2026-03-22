0e271ff57877824ad41da08943e4a36d3c5335b7
Amy <amyjims2103@gmail.com>
2026-03-22 20:07:18 +0530

    Changed App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Donor from "./pages/Donor";
+import Volunteer from "./pages/Volunteer";
+import Receiver from "./pages/Receiver";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {/* 🔹 Temporary Navigation */}
+      <div>
+        <a href="/login">Login</a> | 
+        <a href="/donor">Donor</a> | 
+        <a href="/volunteer">Volunteer</a> | 
+        <a href="/receiver">Receiver</a>
+      </div>
+
+      {/* 🔹 Routes */}
+      <Routes>
+        <Route path="/" element={<h1>Landing Page</h1>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/donor" element={<Donor />} />
+        <Route path="/volunteer" element={<Volunteer />} />
+        <Route path="/receiver" element={<Receiver />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
