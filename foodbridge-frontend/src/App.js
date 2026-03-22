e711176727f327349cb8fef38400da10e2770141
Amy <amyjims2103@gmail.com>
2026-03-22 22:27:38 +0530

    Modified App.js and added PotectedRoutes

--- foodbridge-frontend/src/App.js
+++ foodbridge-frontend/src/App.js
@@ -1,28 +1,52 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Donor from "./pages/Donor";
 import Volunteer from "./pages/Volunteer";
 import Receiver from "./pages/Receiver";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
-      {/* 🔹 Temporary Navigation */}
+
+      {/* Navigation */}
       <div>
-        <a href="/login">Login</a> | 
-        <a href="/donor">Donor</a> | 
-        <a href="/volunteer">Volunteer</a> | 
-        <a href="/receiver">Receiver</a>
+        <Link to="/login">Login</Link> | 
+        <Link to="/donor">Donor</Link> | 
+        <Link to="/volunteer">Volunteer</Link> | 
+        <Link to="/receiver">Receiver</Link>
       </div>
 
-      {/* 🔹 Routes */}
       <Routes>
-        <Route path="/" element={<h1>Landing Page</h1>} />
         <Route path="/login" element={<Login />} />
-        <Route path="/donor" element={<Donor />} />
-        <Route path="/volunteer" element={<Volunteer />} />
-        <Route path="/receiver" element={<Receiver />} />
+
+        <Route
+          path="/donor"
+          element={
+            <ProtectedRoute>
+              <Donor />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/volunteer"
+          element={
+            <ProtectedRoute>
+              <Volunteer />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/receiver"
+          element={
+            <ProtectedRoute>
+              <Receiver />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
