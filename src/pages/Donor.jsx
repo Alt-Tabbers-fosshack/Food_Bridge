e6835ab9a2a64ab10be2873d10e04285f0013221
Srihari-D-vlsi <sriharidileep2@gmail.com>
2026-03-22 19:45:26 +0530

    Added pages

---
+++ src/pages/Donor.jsx
@@ -0,0 +1,5 @@
+const Donor = () => {
+  return <h1>Donor Page</h1>;
+};
+
+export default Donor;
---
+++ src/pages/Volunteer.jsx
@@ -0,0 +1,5 @@
+const Volunteer = () => {
+  return <h1>Volunteer Page</h1>;
+};
+
+export default Volunteer;
