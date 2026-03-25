95d352b7e725993d438f46c30fc90aeb2ad8caf5
Amy <amyjims2103@gmail.com>
2026-03-25 21:55:43 +0530

    Added auth api layer

---
+++ foodbridge-frontend/src/api/auth.api.js
@@ -0,0 +1,8 @@
+import api from "./axios";
+
+export const login = (data) => api.post("/auth/login", data);
+
+export const register = (data) => api.post("/auth/register", data);
+
+export const refreshToken = (refreshToken) =>
+  api.post("/auth/refresh", { refreshToken });
