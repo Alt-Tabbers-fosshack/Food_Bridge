48eb70ee302c8978587c439d2a7f45ce7e087cbd
Amy <amyjims2103@gmail.com>
2026-03-31 17:00:20 +0530

    Removed trailing / in cors

--- foodbridge_backend/settings.py
+++ foodbridge_backend/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'foodbridge_backend.urls'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://food-bridge-six.vercel.app/",
+    "https://food-bridge-six.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
